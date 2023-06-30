3223ff625b45a0b48f0c409b32ff00e8bcc3a50a
lofi-J <jsj2505@naver.com>
2023-06-30 15:32:10 +0900

    fix: .env path

--- server/app.js
+++ server/app.js
@@ -1,12 +1,14 @@
-import express from 'express';
-import path from 'path';
+const express = require('express');
+const path = require('path');
+require('dotenv').config({path: __dirname + '/../.env'});
+
+console.log(process.env.PORT);
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-const __dirname = path.resolve();
 
 app.use(express.static(__dirname + '/../client/build'));
-console.log(__dirname)
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/../client/build/index.html');
 });
