9fa7889a7e496f636233e37ee33e3db3f6e298db
lofi-J <jsj2505@naver.com>
2023-06-30 16:00:03 +0900

    fix: app.js path

--- server/app/app.js
+++ server/app/app.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const path = require('path');
-require('dotenv').config({path: __dirname + '/../.env'});
-
-console.log(process.env.PORT);
+require('dotenv').config({path: __dirname + '/../../.env'});
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + '/../client/build'));
+app.use(express.static(__dirname + '/../../client/build'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/../client/build/index.html');
+    res.sendFile(__dirname + '/../../client/build/index.html');
 });
 
 app.listen(PORT, () => console.log( `http://localhost:${PORT}` ));
